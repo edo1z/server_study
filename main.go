147a22329872b7f8ba517a56c3a5288186da8537
edo1z <edo1z3145@gmail.com>
2017-06-13 23:40:02 +0900

    return daytime server

--- main.go
+++ main.go
@@ -10,34 +10,30 @@ import (
 func main() {
 	service := ":7777"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	checkError(err)
+	chkErr(err, "ResolveTCPAddr")
+
 	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+	chkErr(err, "ListenTCP")
+
 	for {
-		fmt.Println("listen start")
 		conn, err := listener.Accept()
-		fmt.Println("listen now")
 		if err != nil {
-			fmt.Println("listen error")
-			fmt.Println(err)
 			continue
 		}
-		fmt.Println("listen ok")
-		go handleClient(conn)
+		daytime := time.Now().String()
+		_, err = conn.Write([]byte(daytime))
+		chkErr(err, "Write")
+
+		err = conn.Close()
+		chkErr(err, "Close")
 	}
+
 }
 
-func handleClient(conn net.Conn) {
-	fmt.Println("handle start")
-	defer conn.Close()
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime))
-}
-
-func checkError(err error) {
+func chkErr(err error, place string) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
-
+		fmt.Printf("(%s)", place)
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
+		os.Exit(0)
 	}
 }
